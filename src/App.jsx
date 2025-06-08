1cdc59d8d550ffa240afeeb7654ad4bc4fb43371
Narayann1030 <neupanen627@gmail.com>
2025-06-08 11:34:28 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import ToDoList from "./To-Do-List.jsx";
+
+function App() {
+  return (
+  <ToDoList />    
+  );
+}
+export default App;
---
+++ src/To-Do-List.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function ToDoList() {
+  const [tasks, setTasks] = useState(["make a project"]);
+  const taskLists = tasks.map((task, index) => (
+    <li key={index}>
+      {task}
+      <input type="checkbox" name="checkbox" />
+      <button onClick={() => deleteItem(index)}>🗑️</button>
+    </li>
+  ));
+
+  function addTask(formData) {
+    const newTasks = formData.get("task");
+    newTasks.length > 0
+      ? setTasks((prevtasks) => [...prevtasks, newTasks])
+      : null;
+  }
+  function deleteItem(indexToRemove) {
+    setTasks(tasks.filter((_, index) => index !== indexToRemove));
+  }
+
+  return (
+    <>
+      <h1>To Do List</h1>
+      <div className="container">
+        <form action={addTask}>
+          <input type="text" placeholder="Enter a task" name="task" />
+          <button>Add Task</button>
+        </form>
+        <ol>{taskLists}</ol>
+        <hr />
+        {taskLists.length === 1 ? (
+          <p>You have 1 task in hand</p>
+        ) : (
+          <p>You have {taskLists.length} tasks in hand</p>
+        )}
+      </div>
+    </>
+  );
+}
+export default ToDoList;
