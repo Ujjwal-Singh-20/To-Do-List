3e4cc6018098c4dc44741777d95c28c633759eb8
Ujjwal Singh <ujjusingh20@gmail.com>
2025-10-26 17:29:32 +0530

    added dark mode toggle

--- src/To-Do-List.jsx
+++ src/To-Do-List.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import "./index.css";
 
 function ToDoList() {
   const [tasks, setTasks] = useState(["make a project"]);
+  const [darkMode, setDarkMode] = useState(false);
+
   const taskLists = tasks.map((task, index) => (
     <li key={index}>
       {task}
@@ -22,21 +25,27 @@ function ToDoList() {
 
   return (
     <>
-      <h1>TO DO LIST</h1>
-      <div className="container">
-        <form action={addTask}>
-          <input type="text" placeholder="Enter a task" name="task" />
-          <button>Add Task</button>
-        </form>
-        <ol>{taskLists}</ol>
-        <hr />
-        {taskLists.length === 1 ? (
-          <p>You have 1 task in hand</p>
-        ) : (
-          <p>You have {taskLists.length} tasks in list</p>
-        )}
+      <div className={darkMode ? "dark-mode" : "light-mode"}>
+        <h1>TO DO LIST</h1>
+        <button onClick={() => setDarkMode(!darkMode)}>
+          {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </button>
+        <div className="container">
+          <form action={addTask}>
+            <input type="text" placeholder="Enter a task" name="task" />
+            <button>Add Task</button>
+          </form>
+          <ol>{taskLists}</ol>
+          <hr />
+          {taskLists.length === 1 ? (
+            <p>You have 1 task in hand</p>
+          ) : (
+            <p>You have {taskLists.length} tasks in list</p>
+          )}
+        </div>
       </div>
     </>
   );
 }
+
 export default ToDoList;
